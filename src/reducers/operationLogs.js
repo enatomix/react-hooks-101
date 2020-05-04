a2ba862c621f3959d2d23442fbfb7f8ce5970daa
栄佑 <hero.naga0706@gmail.com>
2020-05-04 09:28:20 +0900

    create operation logs

--- src/reducers/operationLogs.js
+++ src/reducers/operationLogs.js
@@ -11,7 +11,7 @@ const operationLogs=(state=[],action)=>{
                 operatedAt:action.operatedAt
             }
             return [operationLog,...state];
-        case DELETE_OPERATION_LOGS:
+        case DELETE_ALL_OPERATION_LOGS:
             return [];
         default:
             return state;
